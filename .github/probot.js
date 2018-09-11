46fa35f4bd78c0df51846219b77aef6690c1103b
Arseniy Ivanov <138289+freeatnet@users.noreply.github.com>
2018-09-11 20:44:31 +0300

    Update probot.js

--- .github/probot.js
+++ .github/probot.js
@@ -1,17 +1,20 @@
 on('issues.opened')
   .filter(context => context.payload.issue.body.match(/This is a request to add a new token to tokenbase/))
   .comment(`
-  <!-- First contact -->
+<!-- First contact -->
 
-Thank you for your request, @{{ sender.login }}! We'll review it as soon as we can. In the meanwhile, here's what to expect:
+Thank you for your request, @{{ issue.user.login }}!
 
-- All communication regarding your request will be posted here. We will not ask you to move conversation to email or a messenger. You can always see all replies on this page.
+**For your safety, please note the following:**
+- All communication regarding your request will be posted publicly in this request. You can check the entire history for the request here, at https://github.com/forkdelta/tokenbase/issues/{{ issue.number }}.
+- Users affiliated with ForkDelta project will have a "Member" badge on their comment. You may also see comments from other users.
+- **There is no payment, it's free!** There is no charge for adding or updating tokens.  If you receive any messages asking for payment, do not respond to them.
+- We will not ask you to move conversation to email or a messenger.
+- We do not provide Tokenbase support over email or direct messages. If you have questions or concerns, please post them here.
+
+Next steps:
 - Please make sure your request follows the [required format](https://github.com/forkdelta/tokenbase/blob/master/.github/ISSUE_TEMPLATE/add-a-new-token.md) and that the information provided is complete and accurate. This will improve the speed of processing your request greatly.
-- Keep an eye on this issue. We will post all follow-up and additional questions here.
+- Keep an eye on this issue. We will post all follow-up and additional questions here, at https://github.com/forkdelta/tokenbase/issues/{{ issue.number }}.
 - You will receive a notification once your request has been reviewed for completeness and accepted.
 - You will receive another notification when the token you requested is about to be added, along with the date when it is added.
-
-Please note:
-- Adding a token to ForkDelta Tokenbase **is free**, we do not charge for this service. If you receive any messages asking for payment, do not respond to them.
-- We will not send or accept email or direct messages related to token addition requests.
-  `);
+`);
