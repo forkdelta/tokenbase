9ccb99660880b895706846a1a9017acdb50143aa
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-09-21 15:15:41 +0300

    Log and continue tests if one of the token files causes a parser exception

--- scripts/test.py
+++ scripts/test.py
@@ -337,14 +337,21 @@ def main(targets, quiet=False):
     failures_count = 0
 
     for target in targets:
+        print("Checking", target)
         try:
             with open(target) as f:
                 content = yaml.safe_load(f.read())
+        except yaml.scanner.ScannerError as exc:
+            test_results.append((target, None, exc))
+            failures_count += 1
+            print("  {} (ERROR - {})".format(exc.__class__.__name__,
+                                             failures_count))
+            print("  Aborted test for {}".format(target))
+            continue
         except FileNotFoundError:
-            print("File not found: {}".format(target))
+            print("  File not found: {}".format(target))
             continue
 
-        print("Checking", target)
         for test in generate_tests(target, content):
             try:
                 retval = test(content)
@@ -394,7 +401,8 @@ def main(targets, quiet=False):
         else:
             print("  {}) Failure/Error: {}: {}".format(
                 idx + 1, outcome.__class__.__name__, outcome))
-            print("        in {}: {}".format(test.__name__, test.__doc__))
+            if test is not None:
+                print("        in {}: {}".format(test.__name__, test.__doc__))
             if not isinstance(outcome, AssertionError):
                 traceback.print_tb(outcome.__traceback__)
         print()
