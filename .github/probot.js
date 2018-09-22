0316adc113a75bffb6f27a09626672c275756d7b
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-09-22 22:02:18 +0300

    Have probot create a project card for new token requests

--- .github/probot.js
+++ .github/probot.js
@@ -4,6 +4,9 @@ const KNOWN_TEMPLATES = {
   "user-support": /This is a user support issue/
 };
 
+const NEW_TOKEN_PROJECT = "Add or Update Token";
+const NEW_TOKEN_COLUMN = "Needs Review";
+
 on("issues.opened")
   .filter(
     // Find issues not matching any templates
@@ -19,7 +22,8 @@ on("issues.opened")
   .filter(context =>
     context.payload.issue.body.match(KNOWN_TEMPLATES["add-a-new-token"])
   )
-  .comment(contents(".github/probot/add-a-new-token-first-contact.md"));
+  .comment(contents(".github/probot/add-a-new-token-first-contact.md"))
+  .createCard({ project: NEW_TOKEN_PROJECT, column: NEW_TOKEN_COLUMN });
 
 on("issues.opened")
   .filter(context =>
