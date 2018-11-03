5e76b18066083678cb6fd5b244a00df0237b21c3
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-11-03 13:41:23 +0300

    Compile tokenbase data into JSON

--- scripts/build.py
+++ scripts/build.py
@@ -1,20 +1,38 @@
+from glob import glob
 import json
-from os import listdir, path
 import yaml
 
+
+def read_entry(fn):
+    with open(fn) as infile:
+        return yaml.safe_load(infile)
+
+
+INDEX_KEYS = ["addr", "decimals", "name", "symbol"]
+
+
+def abridged_entry(entry):
+    return {k: entry[k] for k in INDEX_KEYS}
+
+
 def main():
-    tokens_dir = "tokens"
-    token_file_filter = lambda fname: fname.startswith("0x") and fname.endswith(".yaml")
+    files = sorted(glob("tokens/0x*.yaml"))
 
-    all_tokens = {}
-    for defn_fname in sorted(map(lambda s: s.lower(), filter(token_file_filter, listdir(tokens_dir)))):
-        with open(path.join(tokens_dir, defn_fname), encoding="utf8") as f:
-            defn = yaml.safe_load(f)
+    entries = list(read_entry(fn) for fn in files)
+    for entry in entries:
+        json_fn = "tokens/{}.json".format(entry["addr"])
+        with open(json_fn, "w") as outfile:
+            json.dump(entry, outfile, separators=(',', ':'))
 
-        all_tokens[defn["addr"]] = defn
+    with open("tokens/bundle.json", "w") as outfile:
+        json.dump(entries, outfile, separators=(',', ':'))
 
-    with open("tokens/index.json", "w", encoding="utf8") as outfile:
-        json.dump(all_tokens, outfile, separators=(',', ':'))
+    with open("tokens/index.json", "w") as outfile:
+        json.dump(
+            list(abridged_entry(entry) for entry in entries),
+            outfile,
+            separators=(',', ':'))
+
 
 if __name__ == "__main__":
     main()
